a32bf243723212e4635698c3198b51bc9f6a8639
tech-saurabh382 <saurabhgawali3008@gmail.com>
2023-11-16 18:57:53 +0530

    Adding Js Dom class 2

--- DOM Manipulation/Class-2/index.js
+++ DOM Manipulation/Class-2/index.js
@@ -1,7 +1,49 @@
 
 
-function eventFunction(){
-    console.log('I have clicked on the document');
-}
+// function eventFunction(){
+//     console.log('I have clicked on the document');
+// }
 
-document.addEventListener('click', eventFunction);
+// document.addEventListener('click', eventFunction);
+
+
+// document.removeEventListener('click', eventFunction);
+
+// const content = document.querySelector('#wrapper');
+
+// content.addEventListener('click',function(event){
+//     console.log(event);
+// });
+
+// let links = document.querySelectorAll('a');
+// let thirdLink = links[2];
+
+// thirdLink.addEventListener('click',function(event){
+//     event.preventDefault();
+//     console.log('maze aaya, aacha laga');
+// });
+
+// let myDiv = document.createElement('div');
+
+// function paraStatus (event){
+//     console.log('Para' + event.target.textContent);
+// }
+
+// myDiv.addEventListener('click', paraStatus);
+
+// for(let i = 0; i<=100;i++){
+//     let newElement = document.createElement('p');
+//     newElement.textContent = 'This is para ' + i;
+
+//     myDiv.appendChild(newElement);
+// }
+
+// document.body.appendChild(myDiv);
+
+let element = document.querySelector('#wrapper');
+
+element.addEventListener('click',function(event){
+    if(event.target.nodeName === 'SPAN'){
+        console.log('span par click kia hai' + event.target.textContent);
+    }
+});
