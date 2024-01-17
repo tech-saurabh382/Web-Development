b40fe1cd4317411331d63499adb787334a445714
tech-saurabh382 <saurabhgawali3008@gmail.com>
2024-01-17 17:59:00 +0530

    Adding React Basics Class 5

---
+++ React Basics/React Basics - Class 5/Top-Course-Project/src/components/Cards.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Card from "./Card";
+import { useState } from "react";
+
+const Cards = (props) => {
+    let courses = props.courses;
+    let category = props.category;
+
+    const [likedCourses, setLikedCourses] = useState([]);
+
+    function getCourses() {
+        if(category === "All"){
+            let allCourses = [];
+            Object.values(courses).forEach(array => {
+                array.forEach(courseData => {
+                    allCourses.push(courseData);
+                })
+            })
+            return allCourses;
+        }
+        else{
+            // main srif specific category ka data array pass karuga
+            return courses[category];
+        }
+    }
+
+
+    return (
+        <div className="flex flex-wrap justify-center gap-4 md-4">
+            {
+                getCourses().map((course) => {
+                    return (<Card key={course.id}
+                        course={course}
+                        likedCourses={likedCourses}
+                        setLikedCourses={setLikedCourses} />)
+                })
+            }
+        </div>
+    );
+}
+
+export default Cards;
