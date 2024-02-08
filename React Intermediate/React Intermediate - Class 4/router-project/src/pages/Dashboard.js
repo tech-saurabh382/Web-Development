558c2383e1065a44813c3f28afb342eb27aa4527
tech-saurabh382 <saurabhgawali3008@gmail.com>
2024-02-08 16:01:41 +0530

    Adding styling to router project

---
+++ React Intermediate/React Intermediate - Class 4/router-project/src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div className='flex items-center justify-center h-full text-3xl text-white'>
+            Welcome to StudyNotion!!
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ React Intermediate/React Intermediate - Class 4/router-project/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = ({ isLoggedIn }) => {
+    return (
+        <div className='flex items-center justify-center h-full text-3xl text-white'>
+            Home
+        </div>
+    )
+}
+
+export default Home
