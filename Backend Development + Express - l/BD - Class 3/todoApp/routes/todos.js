81bda9bf18d12b5887985f122deeb38318df0c4c
tech-saurabh382 <saurabhgawali3008@gmail.com>
2024-03-09 20:14:46 +0530

    Adding Backend Class 3

--- Backend Development + Express - l/BD - Class 3/todoApp/routes/todos.js
+++ Backend Development + Express - l/BD - Class 3/todoApp/routes/todos.js
@@ -4,10 +4,14 @@ const router = express.Router();
 //import controller
 const { createTodo } = require("../controllers/createTodo");
 const { getTodo, getTodoById } = require("../controllers/getTodo");
+const { updateTodo } = require("../controllers/updateTodo");
+const { deleteTodo } = require("../controllers/deleteTodo")
 
 //define APi routes
 router.post("/createTodo", createTodo);
 router.get("/getTodos", getTodo);
-router.get("//getTodos/:id", getTodoById);
+router.get("/getTodos/:id", getTodoById);
+router.put("/updateTodo/:id", updateTodo);
+router.delete("/deleteTodo/:id", deleteTodo);
 
 module.exports = router;
