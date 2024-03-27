93d034bd7b7f818769026f2fe87c3ec3f690b8eb
tech-saurabh382 <saurabhgawali3008@gmail.com>
2024-03-27 11:53:45 +0530

    Adding AuthN & AuthZ - Class 1

---
+++ Backend Development + Express - lI/Class 1/AuthApp/Controllers/Auth.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+// signup route handler
+exports.signup = async (req, res) => {
+    try {
+        // get data 
+        const { name, email, password, role } = req.body;
+        // check if user already exist
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({
+                success: true,
+                message: "User already Exists",
+            });
+        }
+
+        // secure password
+        let hashedPassword;
+        try {
+            hashedPassword = await bcrypt.hash(password, 10);
+        }
+        catch (err) {
+            return res.status(500).json({
+                success: false,
+                message: "Error in hashing Password",
+            })
+        }
+
+        // create entry for user
+        const user = await User.create({
+            name, email, password: hashedPassword, role
+        })
+
+        return res.status(200).json({
+            success: true,
+            message: "User Created Successfully",
+        });
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "User cannnot be registered, please try again later",
+        });
+    }
+}
